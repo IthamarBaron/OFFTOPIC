c30957d0e82b840ee07efcbb990e9382e5f3d1a7
Ithamar <57110382+IthamarBaron@users.noreply.github.com>
2025-04-11 23:23:57 +0300

    fixed localhost omg

--- game-server/server.js
+++ game-server/server.js
@@ -542,6 +542,8 @@ function handleVoteResults(code) {
 }
 
 // Start the server
-server.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
